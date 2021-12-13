220ca838342d0b5c00c74b53ba674343059b9408
NSEM Jean Pierre <pidinsem2004@yahoo.fr>
2021-12-13 13:51:08 +0100

    comments

--- src/main/java/ma/enset/Test.java
+++ src/main/java/ma/enset/Test.java
@@ -1,9 +1,14 @@
+/**
+Commentaire 1
+ */
 package ma.enset; 
 import java.util.*; 
 
 public class Test{
 
 public static void main (String...args){
+
+    //commentaire ligne
     System.out.println("Hello World"); 
 }
 
